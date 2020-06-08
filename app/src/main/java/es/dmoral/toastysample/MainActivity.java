1a2a8de22f2cfc3a6dc0c0cc240e473b9cbde8dd
BuYishi <bu-ish@qq.com>
2020-06-08 17:41:06 +0800

    Clean project

--- app/src/main/java/es/dmoral/toastysample/MainActivity.java
+++ app/src/main/java/es/dmoral/toastysample/MainActivity.java
@@ -1,15 +1,14 @@
 package es.dmoral.toastysample;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.StyleSpan;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import es.dmoral.toasty.Toasty;
 
@@ -17,17 +16,17 @@ import static android.graphics.Typeface.BOLD_ITALIC;
 
 /**
  * This file is part of Toasty.
- *
+ * <p>
  * Toasty is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Toasty is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Toasty.  If not, see <http://www.gnu.org/licenses/>.
  */
